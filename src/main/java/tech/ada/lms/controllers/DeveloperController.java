26be7b4493691acbac234f1fd305782eb9dace60
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-03-27 15:54:34 -0300

    fix application

--- src/main/java/tech/ada/lms/controllers/DeveloperController.java
+++ src/main/java/tech/ada/lms/controllers/DeveloperController.java
@@ -2,11 +2,16 @@ package tech.ada.lms.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -18,7 +23,9 @@ public class DeveloperController {
     private static final List<Developer> DEVELOPERS_LIST = new ArrayList<>();
 
     @GetMapping("/create-developer")
-    public String createDeveloper() {
+    public String createDeveloper(Model model) {
+        model.addAttribute("developer",
+                new DeveloperController.Developer());
         return "create-developer";
     }
 
@@ -31,25 +38,47 @@ public class DeveloperController {
     }
 
     @PostMapping("/create-developer")
-    public String createNewDeveloper(@ModelAttribute("developer") Developer developer) {
+    public String createNewDeveloper(@Valid Developer developer, BindingResult bindingResult, Model model) {
+        System.out.println(developer);
+        if (bindingResult.hasErrors()) {
+            return "create-developer";
+        }
         DEVELOPERS_LIST.add(developer);
         return "redirect:/list-developers";
     }
 
     @GetMapping("/delete-developer/{username}")
-    public String deleteDeveloper(@PathVariable("username") String username) {
+    public String deleteDeveloper(@PathVariable("username") String username, Model model) {
         DEVELOPERS_LIST.removeIf(dev -> dev.username.equals(username));
+        model.addAttribute("developers", DEVELOPERS_LIST);
         return "list-developers";
     }
 
     public static final class Developer {
-        private final String username;
-        private final String email;
-        private final String password;
-        private final String bio;
-        private final List<String> careers;
-        private final String preferredLanguage;
-        private final String tShirt;
+
+        @NotBlank
+        @Size(min = 3, max = 20)
+        private String username;
+        @Email
+        @NotBlank
+        private String email;
+        @Size(min = 6, max = 16)
+        private String password;
+        @Size(min = 50, max = 200)
+        @NotBlank
+        private String bio;
+        @Size(min = 1, max = 4)
+        @NotNull
+        private List<String> careers;
+        @NotBlank
+        @NotNull
+        private String preferredLanguage;
+        @NotBlank
+        @NotNull
+        private String tShirt;
+
+        public Developer() {
+        }
 
         public Developer(
                 String username,
@@ -69,32 +98,60 @@ public class DeveloperController {
             this.tShirt = tShirt;
         }
 
-        public String username() {
+        public String getUsername() {
             return username;
         }
 
-        public String email() {
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
             return email;
         }
 
-        public String password() {
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
             return password;
         }
 
-        public String bio() {
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getBio() {
             return bio;
         }
 
-        public List<String> careers() {
+        public void setBio(String bio) {
+            this.bio = bio;
+        }
+
+        public List<String> getCareers() {
             return careers;
         }
 
-        public String preferredLanguage() {
+        public void setCareers(List<String> careers) {
+            this.careers = careers;
+        }
+
+        public String getPreferredLanguage() {
             return preferredLanguage;
         }
 
-        public String tShirt() {
+        public void setPreferredLanguage(String preferredLanguage) {
+            this.preferredLanguage = preferredLanguage;
+        }
+
+        public String gettShirt() {
             return tShirt;
+        }
+
+        public void settShirt(String tShirt) {
+            this.tShirt = tShirt;
         }
 
         @Override
